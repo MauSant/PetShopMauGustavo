3b408917addc9650061561234ec6cd89c9a57f03
MauSant <maubr93@gmail.com>
2020-11-26 11:49:37 -0300

    Cria SobreCarga no construtor de Client

--- src/Loja/Client.java
+++ src/Loja/Client.java
@@ -6,22 +6,37 @@ import java.util.ArrayList;
 
 public class Client {
     private String name;
+    private float moneyBank;
     private ArrayList<Animal> animals;
 
     /*
     * Contructors
      */
 
-    public Client(String name) {
+    //***************************Exemplo SOBRECARGA*********************************
+
+    public Client(String name, float moneyBank) {
         this.name = name;
+        this.moneyBank = moneyBank;
         this.animals = new ArrayList<>();
     }
+
+    public Client(String name) {
+        this.name = name;
+        this.moneyBank = 0;
+        this.animals = new ArrayList<>();
+    }
+    //***************************Exemplo SOBRECARGA*********************************
 
     /*
     * Getters
      */
     public String getName() {
         return name;
+    }
+
+    public float getMoneyBank() {
+        return moneyBank;
     }
 
     public ArrayList<Animal> getAnimals() {
@@ -34,6 +49,10 @@ public class Client {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setMoneyBank(float moneyBank) {
+        this.moneyBank = moneyBank;
     }
 
     public void addAnimals(Animal animal) {
