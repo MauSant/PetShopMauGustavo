6c6cbf0afce8f34fe4cf7ed1e885a0232bde70ae
MauSant <maubr93@gmail.com>
2020-11-26 09:40:58 -0300

    Cria contrutores, getters e setters Basicos, eles podem ser modificados depois

--- src/Loja/Pets/Animal.java
+++ src/Loja/Pets/Animal.java
@@ -5,4 +5,29 @@ import Loja.Loja;
 public class Animal {
     private String name;
 
+    /*
+     * Contrutores
+     */
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    /*
+     * Getters
+     */
+
+    public String getName() {
+        return name;
+    }
+    /*
+     * Setters
+     */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    /*
+     * Methods
+     */
+
 }
